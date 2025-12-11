687d343c335e6d593893652a2fea2ee92b73f6a2
shambhaviprasad2006 <shambhaviprasad2006@gmail.com>
2025-12-12 05:24:28 +0530

    Add functions to show content and display random note

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-let a='hey'
+function showContent(sectionId) {
+var sections = document.getElementsByClassName('content-section');
+for(var i=0; i<sections.length; i++) {
+sections[i].classList.remove('show');
+}
+document.getElementById(sectionId).classList.add('show');
+}
+
+function getRandomNote() {
+var notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+var randomNote = notes[Math.floor(Math.random() * notes.length)];
+document.getElementById('noteDisplay').textContent = 'Random Note: ' + randomNote;
+}
